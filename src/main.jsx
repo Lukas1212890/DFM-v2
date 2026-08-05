784662d476db33edaead4db09657710455d878d7
Lukas1212890 <bednarik@dronetech.cz>
2026-08-05 12:28:23 +0200

    Load mobile horizontal-scroll fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 import './styles.css';
+import './mobile-fixes.css';
 
 const updateSW = registerSW({
   immediate: true,
